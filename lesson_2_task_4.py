1c242e63094df036d358b479f499db3848e48506
utanayno <utanayno@gmail.com>
2023-11-18 00:25:34 +0100

    new

---
+++ lesson_2_task_4.py
@@ -0,0 +1,13 @@
+number = int(input("Введите число: "))
+def fizz_buzz(number):
+    for number in range(1, number+1):
+        if (number % 5 == 0) and (number % 3 == 0):
+            print("FizzBuzz")
+        elif (number % 3 == 0):
+            print("Fizz")
+        elif (number % 5 == 0):
+            print("Buzz")
+        else: 
+            print(number)
+
+fizz_buzz(number)
